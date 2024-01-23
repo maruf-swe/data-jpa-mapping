b81e2ad2a7ee27257c3d744c729d73ae2747fc18
maruf-swe <marufur1847@gmail.com>
2024-01-23 23:58:10 +0600

    entity

---
+++ src/main/java/com/example/datajpamapping/entity/Address.java
@@ -0,0 +1,8 @@
+package com.example.datajpamapping.entity;
+
+public class Address {
+
+    private  Long addressId;
+    private String city;
+    private String addressType;
+}
---
+++ src/main/java/com/example/datajpamapping/entity/Employee.java
@@ -0,0 +1,9 @@
+package com.example.datajpamapping.entity;
+
+@Entity
+public class Employee {
+    private Long empId;
+    private String empName;
+    private Integer empAge;
+
+}
