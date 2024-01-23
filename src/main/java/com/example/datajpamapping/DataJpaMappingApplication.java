c7e2fb13cc3b89d73b07e880bf9ef5b7e7f13939
maruf-swe <marufur1847@gmail.com>
2024-01-23 23:50:04 +0600

    add project

---
+++ src/main/java/com/example/datajpamapping/DataJpaMappingApplication.java
@@ -0,0 +1,13 @@
+package com.example.datajpamapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataJpaMappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataJpaMappingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/datajpamapping/DataJpaMappingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.datajpamapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataJpaMappingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
